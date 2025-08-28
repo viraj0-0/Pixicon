96bf00393fc83194794e823ccc7ad86f8d92c0db
viraj0-0 <prasadviraj67@gmail.com>
2025-08-28 21:42:03 +0530

    Update api.js

--- client1/pixi/src/Authentication/api.js
+++ client1/pixi/src/Authentication/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api =axios.create({
-    baseURL : 'http://localhost:8080/auth'
+    baseURL : 'https://pixicon-backend.onrender.com/auth'
 })
 
 export const googleAuth =(code)=> api.get(`/google?code=${code}`)     
